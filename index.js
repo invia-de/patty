510402d9e7a51508cbfac9037c5b19ed081d66f1
Eric Zieger <eric.zieger@invia.de>
2019-02-06 09:53:14 +0100

    fixes start/build commands, adds transpilation script, adds index.js for im/export

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * add your components here like:
+ *
+ * import Component1 from './dist/components/atoms/Component/Component';
+ * import Component2 from './dist/components/atoms/Component/Component';
+ *
+ * export { Component1, Component2 }
+ */
