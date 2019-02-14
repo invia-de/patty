4e9daa41bc5670fd1a1ce71ea317a1c3fafc866b
Eric Zieger <eric.zieger@invia.de>
2019-02-14 11:45:08 +0100

    compat with upstream/master

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * ! DO NOT USE FOR LEGACY BUILDS !
+ * ! add your legacyBuild import/exports to a src/build-file/bundleName.js !
+ *
+ * add your components here like:
+ *
+ * import Component1 from './dist/components/atoms/Component/Component';
+ * import Component2 from './dist/components/atoms/Component/Component';
+ *
+ * export { Component1, Component2 }
+ */
