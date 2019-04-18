77e047ebdc67a9fe5904a647b22b5a5c75b042b1
Eric Zieger <eric.zieger@invia.de>
2019-04-18 09:28:27 +0200

    use gitlab pages

--- public/service-worker.js
+++ public/service-worker.js
@@ -11,11 +11,11 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
-
 importScripts(
-  "precache-manifest.04e55fb3e16196d6e1f9279c83e8d001.js"
+  'https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js'
 );
+
+importScripts('precache-manifest.04e55fb3e16196d6e1f9279c83e8d001.js');
 
 workbox.clientsClaim();
 
@@ -28,7 +28,6 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
-  
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+workbox.routing.registerNavigationRoute('/index.html', {
+  blacklist: [/^\/_/, /\/[^\/]+\.[^\/]+$/]
 });
