d14574db1542f07812bbae645eae69d729d331ab
Eric Zieger <eric.zieger@invia.de>
2019-05-06 07:30:41 +0200

    removes unused cleverPriceBanner from final libarary files

--- src/legacy/PriceHistory/PriceHistory.js
+++ src/legacy/PriceHistory/PriceHistory.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import Price from '../../components/utilities/Price/Price';
 import NoBreak from '../../components/utilities/NoBreak/NoBreak';
 import DateTime from '../../components/utilities/DateTime/DateTime';
+import Loading from '../../components/atoms/Loading/Loading';
 import styles from './PriceHistory.module.scss';
 import mock from './mock.json';
 import {
@@ -23,31 +24,106 @@ class PriceHistory extends React.Component {
       position: index > 6 ? index - 7 : 0,
       lowestPriceIndex: index > 6 ? index - 7 : 0
     };
+
+    this.onClickPrev = this.onClickPrev.bind(this);
+    this.onClickNext = this.onClickNext.bind(this);
   }
 
   componentDidMount() {
     this.setState({});
   }
 
-  onClickPrev() {}
+  onClickPrev() {
+    this.setState(({ data, position }) => {
+      let newPos = position - 7;
+      return {
+        position: newPos,
+        loading: newPos > data.length - 14 || newPos < 0
+      };
+    }, this.getData);
+  }
 
-  onClickNext() {}
+  onClickNext() {
+    this.setState(({ data, position }) => {
+      let newPos = position + 7;
+      return {
+        position: newPos,
+        loading: newPos > data.length - 14 || newPos < 0
+      };
+    }, this.getData);
+  }
+
+  getData() {
+    if (this.state.loading) {
+      window.setTimeout(() => {
+        this.setState({ loading: false });
+      }, 1500);
+    }
+  }
+
+  addDaysToDate(date, daysToAdd) {
+    date = new Date(date);
+    date.setDate(date.getDate() + daysToAdd);
+    return date;
+  }
+
+  getFiller(fillCount, departureDate, dayModification) {
+    let filler = new Array(fillCount <= 14 ? fillCount : 14)
+      .fill(1)
+      .map((_, i) => {
+        return {
+          loading: true,
+          priceInEuro: null,
+          departureDate: this.addDaysToDate(
+            departureDate,
+            (i + 1) * dayModification
+          )
+        };
+      });
+
+    return dayModification === -1 ? filler.reverse() : filler;
+  }
+
+  getView() {
+    const { data, position } = this.state;
+
+    if (position < 0) {
+      return [
+        ...this.getFiller(~position + 1, data[0].departureDate, -1),
+        ...(position > -14 ? data.slice(0, 14 + position) : [])
+      ];
+    } else if (data.length < position + 14) {
+      return [
+        ...(position < data.length ? data.slice(position) : []),
+        ...this.getFiller(
+          position + 14 - data.length,
+          data[data.length - 1].departureDate,
+          1
+        )
+      ];
+    }
+
+    return data.slice(position, 14 + position);
+  }
 
   render() {
-    let view = this.state.data.slice(
-      this.state.position,
-      14 + this.state.position
-    );
-    const arr = view.map(obj => obj.priceInEuro);
+    let view = this.getView();
+    const arr = view
+      .map(obj => obj.priceInEuro)
+      .filter(price => price !== null);
     const max = Math.max(...arr);
     const min = Math.min(...arr);
     const step = (max - min) / 100;
 
     view = view.map(obj => {
-      obj.className = styles.bar;
-      if (obj.priceInEuro === min) {
+      if (obj.loading) {
+        obj.className = styles.bar_loading;
+      } else if (obj.priceInEuro === min) {
         obj.className = styles.bar_cheapest;
+      } else {
+        obj.className = styles.bar;
       }
+
       return obj;
     });
 
@@ -56,8 +132,17 @@ class PriceHistory extends React.Component {
         <h2>Preisverlauf</h2>
         <div className={styles.chart}>
           {view.map(
-            ({ priceInEuro, currency, duration, className, airport }, i) => {
-              return (
+            (
+              { priceInEuro, currency, duration, className, airport, loading },
+              i
+            ) => {
+              return loading ? (
+                <div className={styles.loading_wrapper}>
+                  <div className={className}>
+                    <Loading />
+                  </div>
+                </div>
+              ) : (
                 <Tooltip
                   key={i}
                   message={
@@ -89,15 +174,14 @@ class PriceHistory extends React.Component {
         </div>
         <div className={styles.axisContainer}>
           <button
+            disabled={this.state.loading}
             className={styles.button}
-            onClick={() =>
-              this.setState(({ position }) => ({ position: position - 7 }))
-            }
+            onClick={this.onClickPrev}
           >
             <IconArrowLeft />
           </button>
           <div className={styles.axis}>
-            {view.map((obj, i) => {
+            {view.map(obj => {
               return (
                 <DateTime
                   className={styles.label}
@@ -108,10 +192,9 @@ class PriceHistory extends React.Component {
             })}
           </div>
           <button
+            disabled={this.state.loading}
             className={styles.button}
-            onClick={() =>
-              this.setState(({ position }) => ({ position: position + 7 }))
-            }
+            onClick={this.onClickNext}
           >
             <IconArrowRight />
           </button>
