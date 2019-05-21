012ea950c061b62174705869de7ddcd0152bb3bd
Eric Zieger <eric.zieger@invia.de>
2019-05-21 14:04:14 +0200

    updates dependencies, adds whitelable theming possibility / basic idea

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,21 +1,6 @@
 const pkg = require('./package.json');
 
 module.exports = {
-  /**
-   * @todo: test if this can be removed after react-styleguidst 9.0.0 release
-   */
-  webpackConfig: require('react-scripts/config/webpack.config'),
-  /**
-   * @todo: test if this can be removed after react-styleguidst 9.0.0 release
-   * @see: https://github.com/styleguidist/react-styleguidist/issues/1247#issuecomment-454644352
-   */
-  dangerouslyUpdateWebpackConfig(webpackConfig) {
-    webpackConfig.output = {
-      ...webpackConfig.output,
-      publicPath: process.env.PUBLIC_URL || ''
-    };
-    return webpackConfig;
-  },
   styles: {
     StyleGuide: {
       '@global *': {
