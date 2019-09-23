147acc040038e217df4681deeeb28480422b86fa
Vitaly Semko <vitaly.semko-extern@invia.de>
2019-09-23 08:45:31 +0200

    FFP-431 workaround for the 3rd-party banner swipe bug

--- src/legacy/ServiceBanner/ServiceBanner.js
+++ src/legacy/ServiceBanner/ServiceBanner.js
@@ -98,7 +98,7 @@ class ServiceBanner extends React.Component {
           ref={this.reactSwipe}
           swipeOptions={{
             auto: this.autoSpeed,
-            speed: 1000
+            speed: agents.length < 3 ? 1 : 1000
           }}
           childCount={agents.length}
         >
